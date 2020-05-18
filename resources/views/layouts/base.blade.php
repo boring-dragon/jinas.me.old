90d88f712e0dd55a4401d111b922176bb4572a31
Mohamed Jinas <j@live.mv>
2020-05-18 15:57:14 +0500

    meta single article title

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Jinas.me</title>
+<title>Jinas.me - {{$post->title ?? ''}}</title>
 
     <meta name="theme-color" content="#011627">
 

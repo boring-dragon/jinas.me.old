55ff0a53615fc558401d88f5b96d0aa0d5975dc2
Mohamed jinas <j@live.mv>
2021-02-18 08:15:21 +0500

    new ui

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -92,17 +92,14 @@ class="  font-sans bg-dark-black text-gray-200 min-h-screen antialiased" :class=
 
 
             <div class="border-t border-gray-800 py-4 flex flex-col">
-                <a href="https://twitter.com/jakzaizzat" title="Twitter" target="_blank" rel="noopener" class="mr-3 text-3xl mb-8 flex items-center">
-                     <svg  class="w-6 h-6" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"/></svg>                     <span class="text-lg ml-3">@jakzaizzat</span>
+                <a href="https://twitter.com/boring_dragon" title="Twitter" target="_blank" rel="noopener" class="mr-3 text-3xl mb-8 flex items-center">
+                     <svg  class="w-6 h-6" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"/></svg>                     <span class="text-lg ml-3">@boring_dragon</span>
                     </a>
-                <a href="mailto:jakz@hey.com" title="Email" target="_blank" rel="noopener"  class="mr-3 text-3xl mb-8 flex items-center">
+                <a href="mailto:j@live" title="Email" target="_blank" rel="noopener"  class="mr-3 text-3xl mb-8 flex items-center">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-                     <span class="text-lg ml-3">jakz@hey.com</span>
+                     <span class="text-lg ml-3">j@live</span>
                 </a>
-                <a class="flex items-center" href="https://feedly.com/i/subscription/feed/https://jakzaizzat.com/rss/" title="RSS" target="_blank" rel="noopener">
-                    <svg class="w-6 h-6" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><circle cx="6.18" cy="17.82" r="2.18"/><path d="M4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z"/></svg>
-                     <span class="text-lg ml-3">/rss</span>
-                </a>
+               
             </div>
 
         </div>

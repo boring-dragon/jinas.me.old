350ef6e0c13e0b9d9352a3e32e57f78b3842cece
Mohamed jinas <j@live.mv>
2021-02-18 08:18:42 +0500

    new ui

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -9,13 +9,14 @@
             <h1 class="gh-title text-center text-3xl font-bold mb-4">{{$article->title}}</h1>
             <p class="gh-post-meta text-center text-sm  text-gray-200"><time datetime="{{$article->created_at->diffforhumans()}}">{{$article->created_at->diffforhumans()}}</time> <span class="bull">&bull;</span> {{read_time($article->body)}}</p>
     
-    
+    @if($article->featured_image)
             <img class="gh-feature-image rounded-lg border border-gray-700 mt-8"
     
                 loading="lazy"
                 src="{{$article->featured_image}}"
                 alt="{{$article->title}}"
             />
+    @endif
         </header>
     
         <div class="gh-content  gh-canvas">

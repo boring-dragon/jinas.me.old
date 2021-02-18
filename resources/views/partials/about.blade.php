ec38e0aaedea0198110d44e758f967edbec46c86
Mohamed jinas <j@live.mv>
2021-02-18 08:12:21 +0500

    new ui

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,7 +1,7 @@
 <div class="container mx-auto px-5 lg:max-w-screen mb-10 pb-5">
     <div class="lg:flex items-center lg:px-10">
         <div class="w-full lg:w-1/6 text-center lg:text-left">
-            <img src="{{$author->avatar}}" class="rounded-full">
+            <img src="{{$author->avatar}}" class="inline-block h-14 w-14 rounded-full">
         </div>
         <div class="lg:pl-5 leading-loose text-text-color w-full lg:w-5/6">
             <div>

a1a77039a1175447e9a7ec6aa4b399ad6c927f14
Mohamed jinas <j@live.mv>
2021-02-18 08:13:19 +0500

    new ui

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,8 +1,5 @@
 <div class="container mx-auto px-5 lg:max-w-screen mb-10 pb-5">
     <div class="lg:flex items-center lg:px-10">
-        <div class="w-full lg:w-1/6 text-center lg:text-left">
-            <img src="{{$author->avatar}}" class="inline-block h-14 w-14 rounded-full">
-        </div>
         <div class="lg:pl-5 leading-loose text-text-color w-full lg:w-5/6">
             <div>
              {!!$author->bio!!}

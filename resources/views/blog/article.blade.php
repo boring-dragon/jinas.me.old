19b24d0701852305b545b07cf4647d9e25fd4524
Mohamed jinas <j@live.mv>
2022-07-08 16:49:52 +0500

    Update article.blade.php

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -7,7 +7,7 @@
     
         <header class="gh-header container mb-16">
             <h1 class="gh-title text-center text-3xl font-bold mb-4">{{$article->title}}</h1>
-            <p class="gh-post-meta text-center text-sm  text-gray-200"><time datetime="{{$article->created_at->diffforhumans()}}">{{$article->created_at->diffforhumans()}}</time> <span class="bull">&bull;</span> {{read_time($article->body)}}</p>
+            <p class="gh-post-meta text-center text-sm  text-gray-200"><time datetime="{{$article->created_at->diffforhumans()}}">{{$article->created_at->diffforhumans()}}</time></p>
     
     @if($article->featured_image)
             <img class="gh-feature-image rounded-lg border border-gray-700 mt-8"
